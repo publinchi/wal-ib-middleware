5894b4524f1b8d327f42b3c6bf9e486d59dc2fbd
jcolmos <jcolmos@cobiscorp.com>
2021-08-04 19:50:25 -0500

    jcos Act

---
+++ OffLine/orchestration-core-ib-spi-transfer/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/transfer/spi/Constants.java
@@ -0,0 +1,32 @@
+package com.cobiscorp.ecobis.orchestration.core.ib.transfer.spi;
+
+public class Constants {
+
+	public static final String CLIENT = "CLIENT";
+    public static final String AUTHORIZATION_SERVER_URL = "AUTH_URL";
+    public static final String GRANT_TYPE = "GRANT_TYPE";
+    public static final String CLIENT_ID = "ID";
+    public static final String LOGIN_SERVER_URL = "/autenticacion";
+    public static final String USERNAME = "USER";
+    public static final String PASSWORD = "PWD";
+    public static final String SCOPE = "SCOPE";
+    public static final String PREDEFINED_REFERENCES_URL = "/referenciaVirtual/predefinida";
+    public static final String CLIENT_SECRET = "SECRET";
+    public static final String ID_CUENTA_ORDENANTE = "ID_CTA_ORD";
+    public static final String QUERY_REFERENCES_URL = "/referenciaVirtual/";
+    public static final String TERCERO_ORDENANTE_URL = "/ordenante/beneficiario/capturarTercero";
+    public static final String SUCCESS_CODE = "HUBP0000";
+    public static final String API_KEY = "APIKEY";
+    public static final String ESTATUS_AUTORIZADA = "AU";
+    public static final String UPDATE_REFERENCES_URL = "/referenciaVirtual/status";
+    public static final String BASE_URL = "BASE_URL";
+    public static final String SERVER_ERROR = "SERVER ERROR";
+    public static final String ACCENDO_EMPTY_VALUE = "N/A";
+    public static final String ERROR_CODE_ON_RFC_CURP = "HUBP0001";
+    
+    public static final String URL_AUTH_SINGLE_TOKEN = "SINGLETURL";
+    public static final String URL_AUTH_SESION_TOKEN = "SESSIONURL";
+    public static final String URL_SPEI_REGISTER = "SPEIURL";
+    public static final String SECURITY_ALGORITH = "SINGALGOTH";
+    public static final String CAT_APP_CLIENT = "CATPPCL";
+}
