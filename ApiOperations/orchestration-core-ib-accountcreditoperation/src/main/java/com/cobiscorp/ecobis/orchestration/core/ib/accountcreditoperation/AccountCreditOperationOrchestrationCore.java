a2034c30be07dea3fbbaed6b35bb66a8ef9eed54
COBISCORP\njimenez <nelson.jimenez@cobistopaz.com>
2023-04-03 13:48:38 -0500

    add trn parameter

--- ApiOperations/orchestration-core-ib-accountcreditoperation/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/accountcreditoperation/AccountCreditOperationOrchestrationCore.java
+++ ApiOperations/orchestration-core-ib-accountcreditoperation/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/accountcreditoperation/AccountCreditOperationOrchestrationCore.java
@@ -98,7 +98,8 @@ public class AccountCreditOperationOrchestrationCore extends SPJavaOrchestration
 		IProcedureRequest reqTMPCentral = (initProcedureRequest(wQueryRequest));		
 		reqTMPCentral.setSpName("cobis..sp_account_credit_operation_central_api");
 		reqTMPCentral.addFieldInHeader(ICOBISTS.HEADER_TARGET_ID, 'S', "central");
-		reqTMPCentral.addFieldInHeader(ICOBISTS.HEADER_TRN, 'N', "18500107");
+		reqTMPCentral.addFieldInHeader(ICOBISTS.HEADER_TRN, 'N', "18500111");
+		reqTMPCentral.addInputParam("@t_trn", ICTSTypes.SQLINT4, "18500111");
 		reqTMPCentral.addInputParam("@i_externalCustomerId", ICTSTypes.SQLINT4, idCustomer);
 		reqTMPCentral.addInputParam("@i_accountNumber",ICTSTypes.SQLVARCHAR, wQueryRequest.readValueParam("@i_accountNumber"));
 		reqTMPCentral.addInputParam("@i_amount",ICTSTypes.SQLNUMERIC, wQueryRequest.readValueParam("@i_amount"));
@@ -147,7 +148,7 @@ public class AccountCreditOperationOrchestrationCore extends SPJavaOrchestration
 			 
 			
 		} else {
-			aBagSPJavaOrchestration.put("50041", "Error account credit operation");
+			aBagSPJavaOrchestration.put("50041", "Error account credit operation orchestration");
 			return;
 		}
 	}
