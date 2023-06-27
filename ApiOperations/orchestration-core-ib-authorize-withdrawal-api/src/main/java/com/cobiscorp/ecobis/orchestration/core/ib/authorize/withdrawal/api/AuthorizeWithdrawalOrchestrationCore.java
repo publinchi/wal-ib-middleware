db2707879edb7c671f912b69e00eacd7746c547b
Santiago Ochoa <sochoa@cobiscorp.com>
2023-06-27 12:34:48 -0500

    Orchestrations Creation

---
+++ ApiOperations/orchestration-core-ib-authorize-withdrawal-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/authorize/withdrawal/api/AuthorizeWithdrawalOrchestrationCore.java
@@ -0,0 +1,298 @@
+/**
+ * 
+ */
+package com.cobiscorp.ecobis.orchestration.core.ib.authorize.withdrawal.api;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+
+import com.cobiscorp.cobis.cis.sp.java.orchestration.ICISSPBaseOrchestration;
+import com.cobiscorp.cobis.cis.sp.java.orchestration.SPJavaOrchestrationBase;
+import com.cobiscorp.cobis.commons.configuration.IConfigurationReader;
+import com.cobiscorp.cobis.commons.log.ILogger;
+import com.cobiscorp.cobis.csp.services.inproc.IOrchestrator;
+import com.cobiscorp.cobis.cts.commons.services.IMultiBackEndResolverService;
+import com.cobiscorp.cobis.cts.domains.ICOBISTS;
+import com.cobiscorp.cobis.cts.domains.ICTSTypes;
+import com.cobiscorp.cobis.cts.domains.IProcedureRequest;
+import com.cobiscorp.cobis.cts.domains.IProcedureResponse;
+import com.cobiscorp.cobis.cts.domains.sp.IResultSetBlock;
+import com.cobiscorp.cobis.cts.domains.sp.IResultSetData;
+import com.cobiscorp.cobis.cts.domains.sp.IResultSetHeader;
+import com.cobiscorp.cobis.cts.domains.sp.IResultSetRow;
+import com.cobiscorp.cobis.cts.dtos.ProcedureRequestAS;
+import com.cobiscorp.cobis.cts.dtos.ProcedureResponseAS;
+import com.cobiscorp.cobis.cts.dtos.sp.ResultSetBlock;
+import com.cobiscorp.cobis.cts.dtos.sp.ResultSetData;
+import com.cobiscorp.cobis.cts.dtos.sp.ResultSetHeader;
+import com.cobiscorp.cobis.cts.dtos.sp.ResultSetHeaderColumn;
+import com.cobiscorp.cobis.cts.dtos.sp.ResultSetRow;
+import com.cobiscorp.cobis.cts.dtos.sp.ResultSetRowColumnData;
+
+/**
+ * @author Sochoa
+ * @since Jun 20, 2023
+ * @version 1.0.0
+ */
+@Component(name = "AuthorizeWithdrawalOrchestrationCore", immediate = false)
+@Service(value = { ICISSPBaseOrchestration.class, IOrchestrator.class })
+@Properties(value = { @Property(name = "service.description", value = "AuthorizeWithdrawalOrchestrationCore"),
+		@Property(name = "service.vendor", value = "COBISCORP"), @Property(name = "service.version", value = "1.0.0"),
+		@Property(name = "service.identifier", value = "AuthorizeWithdrawalOrchestrationCore"),
+		@Property(name = "service.spName", value = "cob_procesador..sp_auth_withdrawal_api")})
+public class AuthorizeWithdrawalOrchestrationCore extends SPJavaOrchestrationBase {
+	
+	private ILogger logger = (ILogger) this.getLogger();
+	private static final String CLASS_NAME = "AuthorizeWithdrawalOrchestrationCore";
+	protected static final String CHANNEL_REQUEST = "8";
+	protected static final String AUTHORIZE_PURCHASE= "AUTHORIZE_WITHDRAWAL";
+	protected static final String MODE_OPERATION = "PYS";
+
+	@Override
+	public void loadConfiguration(IConfigurationReader aConfigurationReader) {
+	}
+	
+	@Override
+	public IProcedureResponse executeJavaOrchestration(IProcedureRequest anOriginalRequest, Map<String, Object> aBagSPJavaOrchestration) {
+		logger.logDebug("Begin flow, AuthorizeWithdrawal starts...");		
+		
+		aBagSPJavaOrchestration.put("anOriginalRequest", anOriginalRequest);
+		
+		
+		IProcedureResponse anProcedureResponse = new ProcedureResponseAS();
+		
+		anProcedureResponse = authorizeWithdrawal(anOriginalRequest, aBagSPJavaOrchestration);
+		
+		return processResponseApi(anProcedureResponse,aBagSPJavaOrchestration);
+	}
+	
+	private IProcedureResponse authorizeWithdrawal(IProcedureRequest aRequest, Map<String, Object> aBagSPJavaOrchestration) {
+		
+		if (logger.isInfoEnabled()) {
+			logger.logInfo(CLASS_NAME + " Entrando en authorizeWithdrawal: ");
+		}
+		
+		IProcedureResponse wAuthValDataLocal = new ProcedureResponseAS();
+		wAuthValDataLocal = valDataLocal(aRequest, aBagSPJavaOrchestration);
+		
+		logger.logInfo(CLASS_NAME + " code resp auth: " + wAuthValDataLocal.getResultSetRowColumnData(2, 1, 1).getValue());
+		if (wAuthValDataLocal.getResultSetRowColumnData(2, 1, 1).getValue().equals("0")){
+			
+			IProcedureResponse wAuthValDataCentral = new ProcedureResponseAS();
+			wAuthValDataCentral = valDataCentral(aRequest, aBagSPJavaOrchestration);
+			
+			if (wAuthValDataCentral.getResultSetRowColumnData(2, 1, 1).getValue().equals("0")) {
+				
+				return wAuthValDataCentral;
+			}	
+		}
+		
+		if (logger.isInfoEnabled()) {
+			logger.logInfo(CLASS_NAME + " Response " + wAuthValDataLocal.toString());
+			logger.logInfo(CLASS_NAME + " Saliendo de authorizeWithdrawal...");
+		}
+
+		return wAuthValDataLocal;
+	}
+	
+	private IProcedureResponse valDataLocal(IProcedureRequest aRequest, Map<String, Object> aBagSPJavaOrchestration) {
+
+		IProcedureRequest request = new ProcedureRequestAS();
+
+		if (logger.isInfoEnabled()) {
+			logger.logInfo(CLASS_NAME + " Entrando en valDataLocal");
+		}
+
+		request.setSpName("cob_atm..sp_auth_purchase_local_api");
+
+		request.addFieldInHeader(ICOBISTS.HEADER_TARGET_ID, ICOBISTS.HEADER_STRING_TYPE,
+				IMultiBackEndResolverService.TARGET_LOCAL);
+		request.setValueFieldInHeader(ICOBISTS.HEADER_CONTEXT_ID, "COBIS");
+		
+		request.addInputParam("@i_externalCustomerId", ICTSTypes.SQLINTN, aRequest.readValueParam("@i_external_customer_id"));
+		request.addInputParam("@i_accountNumber", ICTSTypes.SQLVARCHAR, aRequest.readValueParam("@i_account_number"));
+		
+		request.addOutputParam("@o_account_dock_id", ICTSTypes.SQLVARCHAR, "X");		
+		
+		IProcedureResponse wProductsQueryResp = executeCoreBanking(request);
+		
+		if (logger.isDebugEnabled()) {
+			logger.logDebug("account dock id es " +  wProductsQueryResp.readValueParam("@o_account_dock_id"));
+		}
+		
+		aBagSPJavaOrchestration.put("accountDockId", wProductsQueryResp.readValueParam("@o_account_dock_id"));
+		
+		if (logger.isDebugEnabled()) {
+			logger.logDebug("Response Corebanking DCO: " + wProductsQueryResp.getProcedureResponseAsString());
+		}
+
+		if (logger.isInfoEnabled()) {
+			logger.logInfo(CLASS_NAME + " Saliendo de valDataLocal");
+		}
+
+		return wProductsQueryResp;
+	}
+	
+	private IProcedureResponse valDataCentral(IProcedureRequest aRequest, Map<String, Object> aBagSPJavaOrchestration) {
+
+		IProcedureRequest request = new ProcedureRequestAS();
+
+		if (logger.isInfoEnabled()) {
+			logger.logInfo(CLASS_NAME + " Entrando en valDataCentral");
+		}
+
+		request.setSpName("cob_atm..sp_auth_purchase_central_api");
+
+		request.addFieldInHeader(ICOBISTS.HEADER_TARGET_ID, ICOBISTS.HEADER_STRING_TYPE,
+				IMultiBackEndResolverService.TARGET_CENTRAL);
+		request.setValueFieldInHeader(ICOBISTS.HEADER_CONTEXT_ID, "COBIS");
+		
+		request.addInputParam("@i_externalCustomerId", ICTSTypes.SQLINTN, aRequest.readValueParam("@i_external_customer_id"));
+		request.addInputParam("@i_accountNumber", ICTSTypes.SQLVARCHAR, aRequest.readValueParam("@i_account_number"));
+		
+		IProcedureResponse wProductsQueryResp = executeCoreBanking(request);
+		
+		if (logger.isDebugEnabled()) {
+			logger.logDebug("Response Corebanking DCO: " + wProductsQueryResp.getProcedureResponseAsString());
+		}
+
+		if (logger.isInfoEnabled()) {
+			logger.logInfo(CLASS_NAME + " Saliendo de valDataCentral");
+		}
+
+		return wProductsQueryResp;
+	}
+	
+
+	/*private void registerLogBd(IProcedureResponse reponseAccount, Map<String, Object> aBagSPJavaOrchestration) {
+
+		IProcedureRequest request = new ProcedureRequestAS();
+
+		if (logger.isInfoEnabled()) {
+			logger.logInfo(CLASS_NAME + " Entrando en registerLogBd");
+		}
+
+		request.setSpName("cob_atm..sp_insert_data_dock_api");
+
+		request.addFieldInHeader(ICOBISTS.HEADER_TARGET_ID, ICOBISTS.HEADER_STRING_TYPE,
+				IMultiBackEndResolverService.TARGET_LOCAL);
+		request.setValueFieldInHeader(ICOBISTS.HEADER_CONTEXT_ID, "COBIS");
+		
+		LocalDateTime fechaHoraActual = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        String fechaActual = fechaHoraActual.format(formato);
+		 
+		request.addInputParam("@i_ente", ICTSTypes.SQLINTN, aBagSPJavaOrchestration.get("externalCustomerId").toString());
+		request.addInputParam("@i_cta", ICTSTypes.SQLVARCHAR, aBagSPJavaOrchestration.get("accountNumber").toString());
+		request.addInputParam("@i_fecha_reg", ICTSTypes.SQLVARCHAR, fechaActual);
+		request.addInputParam("@i_fecha_mod", ICTSTypes.SQLVARCHAR, fechaActual);
+		request.addInputParam("@i_modo", ICTSTypes.SQLVARCHAR, "UAS");
+
+		request.addInputParam("@i_cuenta_id", ICTSTypes.SQLVARCHAR, reponseAccount.readValueParam("@o_account_id"));
+		request.addInputParam("@i_request_cd", ICTSTypes.SQLVARCHAR, reponseAccount.readValueParam("@o_request_update_account"));
+		request.addInputParam("@i_estado_cuenta", ICTSTypes.SQLVARCHAR, reponseAccount.readValueParam("@o_account_status"));
+		request.addInputParam("@i_estado_upd", ICTSTypes.SQLVARCHAR, reponseAccount.readValueParam("@o_success"));
+		
+		String message = reponseAccount.readValueParam("@o_response_update_account")!=null?reponseAccount.readValueParam("@o_response_update_account"):reponseAccount.getResultSetRowColumnData(2, 1, 2).getValue();
+		request.addInputParam("@i_response_cd", ICTSTypes.SQLVARCHAR, message);
+		
+		logger.logDebug("Request Corebanking registerLog: " + request.toString());
+		IProcedureResponse wProductsQueryResp = executeCoreBanking(request);
+		
+		if (logger.isDebugEnabled()) {
+			logger.logDebug("Response Corebanking DCO: " + wProductsQueryResp.getProcedureResponseAsString());
+		}
+
+		if (logger.isInfoEnabled()) {
+			logger.logInfo(CLASS_NAME + " Saliendo de registerLogBd");
+		}
+	}*/
+
+	@Override
+	public IProcedureResponse processResponse(IProcedureRequest anOriginalRequest, Map<String, Object> aBagSPJavaOrchestration) {
+		return null;
+	}
+	
+	public IProcedureResponse processResponseApi(IProcedureResponse anOriginalProcedureRes, Map<String, Object> aBagSPJavaOrchestration) {
+		
+		logger.logInfo("processResponseApi [INI] --->" );
+		
+		IProcedureResponse wProcedureResponse = new ProcedureResponseAS();
+
+		Integer codeReturn = anOriginalProcedureRes.getReturnCode();
+		
+		logger.logInfo("return code resp--->" + codeReturn );
+
+		IResultSetHeader metaData = new ResultSetHeader();
+		IResultSetData data = new ResultSetData();
+		metaData.addColumnMetaData(new ResultSetHeaderColumn("success", ICTSTypes.SQLBIT, 5));
+
+		
+		IResultSetHeader metaData2 = new ResultSetHeader();
+		IResultSetData data2 = new ResultSetData();
+		metaData2.addColumnMetaData(new ResultSetHeaderColumn("code", ICTSTypes.SQLINT4, 8));
+		metaData2.addColumnMetaData(new ResultSetHeaderColumn("message", ICTSTypes.SQLVARCHAR, 100));
+
+		
+		if (codeReturn == 0) {
+			
+			logger.logDebug("return code response: " + anOriginalProcedureRes.getResultSetRowColumnData(2, 1, 1));
+			logger.logDebug("Ending flow, processResponse success with code: ");
+			
+			IResultSetRow row = new ResultSetRow();
+			row.addRowData(1, new ResultSetRowColumnData(false, "true"));
+			data.addRow(row);
+			
+			IResultSetRow row2 = new ResultSetRow();
+			row2.addRowData(1, new ResultSetRowColumnData(false, "0"));
+			row2.addRowData(2, new ResultSetRowColumnData(false, "Success"));
+			data2.addRow(row2);
+				
+			/*} else {
+				logger.logDebug("Ending flow, processResponse error");
+				
+				String success = anOriginalProcedureRes.getResultSetRowColumnData(1, 1, 1).isNull()?"false":anOriginalProcedureRes.getResultSetRowColumnData(1, 1, 1).getValue();
+				String code = anOriginalProcedureRes.getResultSetRowColumnData(2, 1, 1).isNull()?"400218":anOriginalProcedureRes.getResultSetRowColumnData(2, 1, 1).getValue();
+				String message = anOriginalProcedureRes.getResultSetRowColumnData(2, 1, 2).isNull()?"Service execution error":anOriginalProcedureRes.getResultSetRowColumnData(2, 1, 2).getValue();
+				
+				IResultSetRow row = new ResultSetRow();
+				row.addRowData(1, new ResultSetRowColumnData(false, success));
+				data.addRow(row);
+				
+				IResultSetRow row2 = new ResultSetRow();
+				row2.addRowData(1, new ResultSetRowColumnData(false, code));
+				row2.addRowData(2, new ResultSetRowColumnData(false, message));
+				data2.addRow(row2);
+			}*/
+		} else {
+			
+			logger.logDebug("Ending flow, processResponse failed with code: ");
+			
+			IResultSetRow row = new ResultSetRow();
+			row.addRowData(1, new ResultSetRowColumnData(false, "false"));
+			data.addRow(row);
+			
+			IResultSetRow row2 = new ResultSetRow();
+			row2.addRowData(1, new ResultSetRowColumnData(false, codeReturn.toString()));
+			row2.addRowData(2, new ResultSetRowColumnData(false, anOriginalProcedureRes.getMessage(1).getMessageText()));
+			data2.addRow(row2);
+		}
+
+		IResultSetBlock resultsetBlock2 = new ResultSetBlock(metaData2, data2);
+		IResultSetBlock resultsetBlock = new ResultSetBlock(metaData, data);
+
+		wProcedureResponse.addResponseBlock(resultsetBlock);
+		wProcedureResponse.addResponseBlock(resultsetBlock2);
+		
+		/*registerLogBd(anOriginalProcedureRes, aBagSPJavaOrchestration);*/
+		
+		return wProcedureResponse;		
+	}
+}
