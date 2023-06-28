9d31a05a981e833ba7749f3d89015ddcbd272bf1
Jose Olmos <jcolmos@cobiscorp.com>
2023-06-28 13:27:09 +0000

    Revert "Nuevos archivos praxys spei"

--- Project_SG_IB/Wsdl_InternetBanking/Code/WS/src/com/cobiscorp/cobis/cts/sdf/dto2/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://dto2.sdf.cts.cobis.cobiscorp.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.cobiscorp.cobis.cts.sdf.dto2;
