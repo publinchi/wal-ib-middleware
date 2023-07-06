364066f3f648f2ecccd8b3b1d351ec368b87ab0e
dcollaguazo <danilo.collaguazo@cobistopaz.com>
2023-07-06 12:33:26 -0500

    DCO: Cambios SG Update card

--- Project_SG_IB/ApiOperartions/Code/CTS/COBISCorp.eCOBIS.DataContractOperations.DTO/src/cobiscorp/ecobis/datacontractoperations/dto/CardApplicationResponse.java
+++ Project_SG_IB/ApiOperartions/Code/CTS/COBISCorp.eCOBIS.DataContractOperations.DTO/src/cobiscorp/ecobis/datacontractoperations/dto/CardApplicationResponse.java
@@ -28,36 +28,8 @@ import java.util.HashMap;
 
 
 public class CardApplicationResponse implements Serializable {
-  private String cardId;
-  private String personId;
-  private String accountId;
-  private String assignmentDate;
   private Boolean success;
   private Response response;
-  public String getCardId(){
-    return this.cardId;
-  }
-  public void setCardId(String cardId ){
-    this.cardId=cardId;
-  }
-  public String getPersonId(){
-    return this.personId;
-  }
-  public void setPersonId(String personId ){
-    this.personId=personId;
-  }
-  public String getAccountId(){
-    return this.accountId;
-  }
-  public void setAccountId(String accountId ){
-    this.accountId=accountId;
-  }
-  public String getAssignmentDate(){
-    return this.assignmentDate;
-  }
-  public void setAssignmentDate(String assignmentDate ){
-    this.assignmentDate=assignmentDate;
-  }
   public Boolean isSuccess(){
     return this.success;
   }
