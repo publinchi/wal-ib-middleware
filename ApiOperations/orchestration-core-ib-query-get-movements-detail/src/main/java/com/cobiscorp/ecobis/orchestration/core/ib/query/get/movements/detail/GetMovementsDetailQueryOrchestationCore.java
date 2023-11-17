7841e61bc14a06e0a94789dd44b1bd7446888107
COBISCORP\njimenez <nelson.jimenez@cobistopaz.com>
2023-11-17 12:44:27 -0500

    solucion conflicto 2

--- ApiOperations/orchestration-core-ib-query-get-movements-detail/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/query/get/movements/detail/GetMovementsDetailQueryOrchestationCore.java
+++ ApiOperations/orchestration-core-ib-query-get-movements-detail/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/query/get/movements/detail/GetMovementsDetailQueryOrchestationCore.java
@@ -743,14 +743,12 @@ public class GetMovementsDetailQueryOrchestationCore extends SPJavaOrchestration
 
 					if (type_auth.equals("WITHDRAWAL")) {
 						movementType = "ATM_DEBIT";
-					
+					}
 					} else if (type_auth.equals("PURCHASE")) {
 						movementType = "PURCHASE_AT_STORE";
 					} else {
 						movementType = type_auth;
-					}					}
-
-				if (operationType.equals("C")) {
+					}					if (operationType.equals("C")) {
 					String copysourceOwnerName = sourceOwnerName;
 					String copydestinyOwnerName = destinyOwnerName;
 					String copysourceAccountNumber = sourceAccountNumber;
