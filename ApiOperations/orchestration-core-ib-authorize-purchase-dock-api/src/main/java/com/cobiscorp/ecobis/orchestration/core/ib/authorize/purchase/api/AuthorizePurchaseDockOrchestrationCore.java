a5cff77dcbfbe47b59c105c2ad99910d57199ed7
COBISCORP\njimenez <nelson.jimenez@cobistopaz.com>
2024-01-19 09:37:50 -0500

    HU https://dev.azure.com/cobiscorp/WAL/_workitems/edit/965366

--- ApiOperations/orchestration-core-ib-authorize-purchase-dock-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/authorize/purchase/api/AuthorizePurchaseDockOrchestrationCore.java
+++ ApiOperations/orchestration-core-ib-authorize-purchase-dock-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/authorize/purchase/api/AuthorizePurchaseDockOrchestrationCore.java
@@ -387,6 +387,7 @@ public class AuthorizePurchaseDockOrchestrationCore extends SPJavaOrchestrationB
 		request.addInputParam("@i_seq", ICTSTypes.SQLINTN, (String) aBagSPJavaOrchestration.get("seq"));
 		request.addInputParam("@i_reentry", ICTSTypes.SQLVARCHAR, (String) aBagSPJavaOrchestration.get("reentry"));
 		request.addInputParam("@i_exe_status", ICTSTypes.SQLVARCHAR, executionStatus);
+		request.addInputParam("@i_movementId", ICTSTypes.SQLINTN, aBagSPJavaOrchestration.containsKey("@o_ssn_host")?aBagSPJavaOrchestration.get("@o_ssn_host").toString():null);
 		
 		logger.logDebug("Request Corebanking registerLog: " + request.toString());
 		
