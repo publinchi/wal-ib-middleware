ba28147b091cb79cd095a6d1fb04978187ef8de7
bmurillo <boris.murillo@cobistopaz.com>
2024-04-11 08:30:26 -0500

    added change to validation concepto

--- ApiOperations/orchestration-core-ib-transfer-third-party-account-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/transfer/third/party/account/api/TransferThirdPartyAccountApiOrchestationCore.java
+++ ApiOperations/orchestration-core-ib-transfer-third-party-account-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/transfer/third/party/account/api/TransferThirdPartyAccountApiOrchestationCore.java
@@ -974,7 +974,16 @@ private IProcedureResponse findCardByPanConector(IProcedureRequest anOriginalReq
 		if (logger.isInfoEnabled())
 			logger.logInfo(CLASS_NAME + "Ejecutando transferencia a terceros CORE COBIS" + request);
 
+<<<<<<< HEAD
 		IProcedureRequest anOriginalRequest = new ProcedureRequestAS();
+=======
+		String concepto = request.readValueParam("@i_concepto");
+		if(concepto == null || concepto.trim().isEmpty()){
+			concepto = "Transferencia Cashi";
+		}
+
+		IProcedureRequest anOriginalRequest = request;
+>>>>>>> cf1c8662 (added change to validation concepto)
 		anOriginalRequest.setValueFieldInHeader(ICOBISTS.HEADER_CONTEXT_ID, COBIS_CONTEXT);
 		anOriginalRequest.addFieldInHeader(ICOBISTS.HEADER_TARGET_ID, ICOBISTS.HEADER_STRING_TYPE,
 				IMultiBackEndResolverService.TARGET_CENTRAL);
@@ -1044,7 +1053,7 @@ private IProcedureResponse findCardByPanConector(IProcedureRequest anOriginalReq
 		
 		anOriginalRequest.addInputParam("@i_val", ICTSTypes.SQLMONEY, request.readValueParam("@i_val"));
 		
-		anOriginalRequest.addInputParam("@i_concepto", ICTSTypes.SQLVARCHAR, request.readValueParam("@i_concepto"));
+		anOriginalRequest.addInputParam("@i_concepto", ICTSTypes.SQLVARCHAR, concepto);
 		
 		if (request.readValueParam("@i_reference_number") != null)
 			anOriginalRequest.addInputParam("@i_reference_number", ICTSTypes.SQLVARCHAR, request.readValueParam("@i_reference_number"));	
