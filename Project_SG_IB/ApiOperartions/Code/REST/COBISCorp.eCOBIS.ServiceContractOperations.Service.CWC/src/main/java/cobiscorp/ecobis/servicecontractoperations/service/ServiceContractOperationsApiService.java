987bcc2f33eec69760a4ebb1ffbe28262341c93c
jcolmos <jcolmos@cobistopaz.com>
2024-04-11 10:52:15 -0600

    Validacion nulable

--- Project_SG_IB/ApiOperartions/Code/REST/COBISCorp.eCOBIS.ServiceContractOperations.Service.CWC/src/main/java/cobiscorp/ecobis/servicecontractoperations/service/ServiceContractOperationsApiService.java
+++ Project_SG_IB/ApiOperartions/Code/REST/COBISCorp.eCOBIS.ServiceContractOperations.Service.CWC/src/main/java/cobiscorp/ecobis/servicecontractoperations/service/ServiceContractOperationsApiService.java
@@ -1315,11 +1315,15 @@ public class ServiceContractOperationsApiService implements IServiceContractOper
 										dto.setResponse(resultSetMapper.getString(4));
 										dto.setReason(resultSetMapper.getString(5));
 										dto.setAvailable_limit(resultSetMapper.getString(6));
-										int auto = resultSetMapper.getInteger(7);										
-								        String numeroCadena = String.valueOf(auto);	
-								        int inicio = Math.max(numeroCadena.length() - 6, 0);
-								        String numeroCortado = numeroCadena.substring(inicio);	
-										dto.setAuthorization_code(Integer.parseInt(numeroCortado));
+										if(resultSetMapper.getString(7)!=null) {
+										int auto = resultSetMapper.getInteger(7);	
+										String numeroCadena = String.valueOf(auto);											
+										if(auto>0 && numeroCadena!=null) {						
+								         int inicio = Math.max(numeroCadena.length() - 6, 0);
+								         String numeroCortado = numeroCadena.substring(inicio);	
+										 dto.setAuthorization_code(Integer.parseInt(numeroCortado));
+										}
+										}
 										dto.setSeq(resultSetMapper.getString(8));
 										
 										
