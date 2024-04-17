2dd3da342488be0e36c48593bf221b413b8bed83
ERIC GALICIA LOZANO <egalicia@cobiscorp.com>
2024-04-17 08:26:05 -0600

    EGL - S971107 - Ajuste de validación de acreditación

--- ApiOperations/orchestration-core-ib-update-card-dock-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/update/card/dock/api/UpdateCardDockOrchestrationCore.java
+++ ApiOperations/orchestration-core-ib-update-card-dock-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/update/card/dock/api/UpdateCardDockOrchestrationCore.java
@@ -249,7 +249,7 @@ public class UpdateCardDockOrchestrationCore extends SPJavaOrchestrationBase {
 		if (logger.isDebugEnabled()) 
 			 logger.logDebug("accreditation_3: " + accreditation);
 		
-		if (accreditation == "Y") 
+		if (accreditation.trim().equals("Y")) 
 			accountAccreditation(aRequest,  aBagSPJavaOrchestration);
 
 		if (logger.isInfoEnabled()) {
