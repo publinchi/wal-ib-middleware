68cdf41abe487edf126cebf35d2647b67246516d
bmurillo <boris.murillo@cobistopaz.com>
2024-05-23 14:21:32 -0500

    eliminado debug

--- ApiOperations/orchestration-core-ib-authorize-reversal-dock-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/authorize/reversal/api/AuthorizeReversalDockOrchestrationCore.java
+++ ApiOperations/orchestration-core-ib-authorize-reversal-dock-api/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/authorize/reversal/api/AuthorizeReversalDockOrchestrationCore.java
@@ -304,11 +304,7 @@ public class AuthorizeReversalDockOrchestrationCore extends OfflineApiTemplate {
 		String pinpad = aRequest.readValueParam("@i_pinpad");
 	    String dest_asset_code = aRequest.readValueParam("@i_exchange_rate_dest_asset_code");
 		String date_time_gmt = aRequest.readValueParam("@i_exchange_rate_date_time_gmt");
-		String final_billing_value = aRequest.readValueParam("@i_exchange_rate_final_billing_value");
-		String transaction_type_indicator = aRequest.readValueParam("@i_transaction_type_indicator");
-		logger.logInfo("transactionType::: " + transaction_type_indicator);
-		
-		
+		String final_billing_value = aRequest.readValueParam("@i_exchange_rate_final_billing_value");	
 		
 		if (values_3 != null && !values_3.isEmpty() && !isNumeric(values_3)) {
 			values_3 = "";
