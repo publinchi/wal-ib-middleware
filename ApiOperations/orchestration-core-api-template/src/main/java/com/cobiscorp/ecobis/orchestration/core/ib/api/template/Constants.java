9a7089e5f40f03d77054277c9a7a9522c01bd7ce
sflores <sflores@cobiscorp.com>
2025-02-07 16:33:42 -0500

    RM_259834 - Parametros faltantes IDC - TOPAZ

---
+++ ApiOperations/orchestration-core-api-template/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/api/template/Constants.java
@@ -0,0 +1,28 @@
+package com.cobiscorp.ecobis.orchestration.core.ib.api.template;
+
+public class Constants {
+    public static final String CASHI = "CASHI";
+    public static final String MXN = "MXN";
+    public static final String CREDIT_AT_STORE = "CREDIT_AT_STORE";
+    public static final String ATM_DEBIT = "ATM_DEBIT";
+    public static final String DEBIT_AT_STORE = "DEBIT_AT_STORE";
+    public static final String PURCHASE_AT_STORE = "PURCHASE_AT_STORE";
+    public static final String PURCHASE_ONLINE = "PURCHASE_ONLINE";
+    public static final String TRANSACTION_SUCCESS = "TRANSACTION_SUCCESS";
+    public static final String P2P_CREDIT = "P2P_CREDIT";
+    public static final String P2P_DEBIT = "P2P_DEBIT";
+    public static final String CARD_DELIVERY_FEE = "CARD_DELIVERY_FEE";
+    public static final String COMMISSION = "COMMISSION";
+    public static final String BONUS = "BONUS";
+    public static final String SPEI_CREDIT = "SPEI_CREDIT";
+    public static final String SPEI_DEBIT = "SPEI_DEBIT";
+    public static final String RETIRO_ATM = "RETIRO_ATM";
+    public static final String SPEI_RETURN = "SPEI_RETURN";
+    
+    public static final String AUTHORIZE_PURCHASE = "Authorize Purchase";
+    public static final String AUTHORIZE_WITHDRAWAL = "Authorize Withdrawal";
+    public static final String AUTHORIZE_DEPOSIT = "Authorize Deposit";
+    public static final String AUTHORIZE_PURCHASE_DOCK = "Authorize Purchase Dock";
+    public static final String AUTHORIZE_WITHDRAWAL_DOCK = "Authorize Withdrawal Dock";
+    public static final String AUTHORIZE_DEPOSIT_DOCK = "Authorize Deposit Dock";
+}
