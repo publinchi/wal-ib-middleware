85c17077a347bd37e703990723f9d517a8ece70f
jcolmos <jcolmos@cobistopaz.com>
2025-05-27 19:49:34 -0600

    Actualizacion interfase core

--- ApiOperations/orchestration-core-ib-account-reversal-operation/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/accountreversalcreditoperation/AccountReversalOperationOrchestrationCore.java
+++ ApiOperations/orchestration-core-ib-account-reversal-operation/src/main/java/com/cobiscorp/ecobis/orchestration/core/ib/accountreversalcreditoperation/AccountReversalOperationOrchestrationCore.java
@@ -43,6 +43,7 @@ import com.cobiscorp.ecobis.ib.application.dtos.ServerResponse;
 import com.cobiscorp.ecobis.ib.orchestration.base.commons.Utils;
 import com.cobiscorp.ecobis.orchestration.core.ib.api.template.OfflineApiTemplate;
 import com.cobiscorp.ecobis.orchestration.core.ib.api.template.Constants;
+import com.cobiscorp.ecobis.ib.orchestration.interfaces.ICoreServer;
 
 /**
  * @author jolmos
